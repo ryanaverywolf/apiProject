c9fd2f9994b78ab59377efdcdcc1a7f3e1dbe5a2
Ryan Wolf <ryanaverywolf@gmail.com>
2019-01-23 13:06:11 -0600

    secondcommit

--- main.js
+++ main.js
@@ -10,24 +10,28 @@ fetch("https://ghibliapi.herokuapp.com/films")
 console.log(title);
     for(t of title) {
         let listItem = document.createElement("li");
-        listItem.innerHTML = "<p>"+ t.title + "</p>";
-        ghibliTitleList.appendChild(listItem);
+        listItem.innerHTML = "<p>"+"Movie:  " + t.title + ",  Release Date:  " + t.release_date + ",  Directed by:  "+ t.director + "</p>";
+        ghibliTitleList.appendChild(listItem);  
+
+        // let listItem = document.createElement("li");
+        // listItem.innerHTML = "<p>"+ t.release_date + "</p>";
+        // ghibliReleaseDate.appendChild(listItem);
     }
 });
 
-var ghibliReleaseDate = document.querySelector("ul");
+// var ghibliReleaseDate = document.querySelector("ul");
 
-fetch("https://ghibliapi.herokuapp.com/films")
+// fetch("https://ghibliapi.herokuapp.com/films")
 
-.then(function(response) { 
-    return response.json(); 
-})
-.then(function(json) {
-    let release_date = json;
-console.log(release_date);
-    for(d of release_date) {
-        let listItem = document.createElement("li");
-        listItem.innerHTML = "<p>"+ d.release_date + "</p>";
-        ghibliReleaseDate.appendChild(listItem);
-    }
-});
+// .then(function(response) { 
+//     return response.json(); 
+// })
+// .then(function(json) {
+//     let release_date = json;
+// console.log(release_date);
+//     for(d of release_date) {
+//         let listItem = document.createElement("li");
+//         listItem.innerHTML = "<p>"+ d.release_date + "</p>";
+//         ghibliReleaseDate.appendChild(listItem);
+//     }
+// });
